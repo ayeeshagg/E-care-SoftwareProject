52337317bff9bdd9aa6cb03c205a3afe608234c8
Prionty-Tarafder <159806800+Prionty-Tarafder@users.noreply.github.com>
2024-11-23 00:40:17 +0600

    Urls added

--- ecare/urls.py
+++ ecare/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from hospital.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('', home, name='home'),
+    path('signup/', SignUp, name='signup'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+    path('shopmedicine/', shopmedicine, name='shopmedicine'),
+    path('medicine_details/<int:medicine_id>/', medicine_details, name='medicine_details'),
+
 ]
