6ef2cc360805f3d7758741334e777cf5d8859d62
Prionty-Tarafder <159806800+Prionty-Tarafder@users.noreply.github.com>
2024-11-23 22:55:06 +0600

    Update admin.py

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Medicine)
+admin.site.register(Doctor)
+admin.site.register(Hospital)
+admin.site.register(Schedule)
+admin.site.register(Appointment)
