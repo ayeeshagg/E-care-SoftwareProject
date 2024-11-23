4e55e67a03b2e796794273c41a63bf1254ca0da4
Prionty-Tarafder <159806800+Prionty-Tarafder@users.noreply.github.com>
2024-11-23 22:52:48 +0600

    new order model

--- hospital/models.py
+++ hospital/models.py
@@ -48,4 +48,19 @@ class Cart(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.medicine.name}"
-       
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('completed', 'Completed'),
+        ('cancelled', 'Cancelled'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    medicines = models.ManyToManyField(Medicine, through='OrderItem', related_name='orders')
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    ordered_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')  
+
+    def __str__(self):
+        return f"{self.user.username} - {self.total}"
