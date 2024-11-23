5bb5f22c8031fbe79f0c5dfe97b226a92a8f699d
effat14 <21201086@uap-bd.edu>
2024-11-23 22:58:51 +0600

    admin fixed

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
 admin.site.register(Medicine)
@@ -6,3 +7,7 @@ admin.site.register(Doctor)
 admin.site.register(Hospital)
 admin.site.register(Schedule)
 admin.site.register(Appointment)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Profile)
+admin.site.register(Cart)
